7ec82015d3502220d6ff0ab0ff61a8e198add74c
Khemraj <khemrajgupta10@gmail.com>
2024-11-30 16:02:21 +0530

    added github pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from 'react';
-import Link from 'next/link';
 import './Navbar.scss';
 
 const Navbar = () => {
@@ -15,12 +14,12 @@ const Navbar = () => {
     <nav className='flex justify-around items-center px-4 py-8 relative bg-[#043873] text-white'>
       <div className='text-2xl font-bold'>whitepace</div>
       <div className='desktopNavLinks'>
-        <Link href="/products">Products</Link>
-        <Link href="/solutions">Solutions</Link>
-        <Link href="/resources">Resources</Link>
-        <Link href="/pricing">Pricing</Link>
-        <Link href="/login"className='bg-[#f7dc92] text-black px-3 py-2 rounded'>Login</Link>
-        <Link href="/trywhitepace" className='bg-[#588ff5] px-4 py-2 rounded' >Try Whitepace free →</Link>
+        <a href="/products">Products</a>
+        <a href="/solutions">Solutions</a>
+        <a href="/resources">Resources</a>
+        <a href="/pricing">Pricing</a>
+        <a href="/login"className='bg-[#f7dc92] text-black px-3 py-2 rounded'>Login</a>
+        <a href="/trywhitepace" className='bg-[#588ff5] px-4 py-2 rounded' >Try Whitepace free →</a>
       </div>
       {/* Mobile Menu Toggle */}
       {/* <div className={setIsMenuOpen ? 'display: flex': 'display: None'}
