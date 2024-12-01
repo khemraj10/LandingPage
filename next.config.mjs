c2219d4a7b2141ca667610a9d7f836fb71080dbd
Khemraj <khemrajgupta10@gmail.com>
2024-12-01 12:17:00 +0530

    changes to next mjs file and package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
-const isProd = process.env.NODE_ENV === 'production';
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? '/your-repository-name/' : '',
-  basePath: isProd ? '/your-repository-name' : '',
   output: 'export'
 };
 
